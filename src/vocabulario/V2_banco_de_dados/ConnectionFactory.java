82cfffcb4247c1ae1b8438611f4077355a6231eb
Miguel <945507756m@gmail.com>
2023-06-12 19:53:19 -0300

    build - add new connection to database oracle

--- src/vocabulario/V2_banco_de_dados/ConnectionFactory.java
+++ src/vocabulario/V2_banco_de_dados/ConnectionFactory.java
@@ -10,21 +10,26 @@ public class ConnectionFactory {
 			// step1 load the driver class
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 
-			Connection con = DriverManager.getConnection("jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL", "tm_miguel_lima", "101204");
+			Connection con = DriverManager.getConnection("jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL","tm_miguel_lima", "101204");
 
 			// step3 create the statement object
 			Statement stmt = con.createStatement();
 
 			// step4 execute query
-			ResultSet rs = stmt.executeQuery("select * from t_voc_vocabulary");
+			ResultSet rs = stmt.executeQuery("select * from t_voc_dictionary");
 			while (rs.next())
-				System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3));
+//				System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3)); // todos os elementos da tabela
+
+//			System.out.println(rs.getInt(1)); // somente o id
+				System.out.println(rs.getString(2)); // somente a tradução
+//			System.out.println(rs.getString(3)); // somente a palavra em ingles
 
 			// step5 close the connection object
 			con.close();
 
 		} catch (Exception e) {
-			System.out.println("Nao foi possviel conectar ao banco de dados - erro: " + e);
+			System.out.println("Nao foi possviel conectar ao banco de dados - erro: ");
+			System.out.println(e);
 		}
 
 	}
